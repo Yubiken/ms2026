9fb75440c11431ab01c5481198b99dd651d05d66
Sebastian Cur <sebastiancur97@gmail.com>
2026-03-05 19:42:55 +0100

    deploy

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,9 +23,11 @@ origins = [
     "http://127.0.0.1:5173",
 ]
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],  # tymczasowo
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
